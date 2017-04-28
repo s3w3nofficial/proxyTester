c26c038710236ce3e6c2dc97de2916ef4d7e3fda
Tomas Pelak <s3w3nofficial@gmail.com>
2017-04-28 11:34:08 +0200

    some changes

--- checkCrawlerd.py
+++ checkCrawlerd.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
 
+import os
+
 def check_file_crawlerd(ip): 
 	with open("crawlerd.txt") as f:
 	    lines = f.readlines()
@@ -14,13 +16,25 @@ def check_file_crawlerd(ip):
 	return False
 
 def write_to_crawlerd(ip):
-	with open("crawlerd.txt", "a") as f:
-	    f.write(ip) 
+	d = "crawlerd.txt"
+	if not os.path.exists(d):
+		    open(d, 'w+')
+	else:
+		with open(d, "a") as f:
+		    	f.write(ip+"\n") 
 
 def write_to_working(ip, port):
-	with open("working.txt", "a") as f:
-		f.write(ip, port)
+	d = "working.txt"
+	if not os.path.exists(d):
+		    open(d, 'w+')
+	else:
+		with open(d, "a") as f:
+			f.write(ip + " " + str(port) + "\n")
 
 def write_to_not_working(ip, port):
-	with open("notworking.txt", "a") as f:
-		f.write(ip, port)
+	d = "notworking.txt"
+	if not os.path.exists(d):
+		    open(d, 'w+')
+	else:
+		with open(d, "a") as f:
+			f.write(ip + " " + str(port) + "\n")
