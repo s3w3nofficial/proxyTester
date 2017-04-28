c0c79a96326c597d375ce88242b1b3306bbd2755
Tomas Pelak <s3w3nofficial@gmail.com>
2017-04-28 16:14:57 +0200

    file getProxies updated

--- getProxies.py
+++ getProxies.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
 
+import urllib
+import re
+import sys
+
 def get_working_proxies():
 	with open("working.txt") as f:
 		lines = f.readlines()
@@ -24,3 +28,18 @@ def get_not_working_proxies():
 		otp.append(line)
 	
 	return otp
+
+def check_proxy(ip, port):
+	try:
+		proxy = {'http' : 'http//' + ip + ':' + str(port)}
+		proxy = urllib.urlopen("http://httpbin.org/ip", proxies=proxy).read()
+		proxy = re.findall(r'"([^"]*)"', proxy)
+		if ip in proxy:
+			return True
+		else:
+			return False
+		except KeyboardInterrupt:
+			print "Ctrl+c was pressed. Exiting!!!"
+			sys.exit(0)
+		except:
+			return "connection lost"
