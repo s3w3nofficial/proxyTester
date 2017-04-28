84e98803250869846cfeada10fb21bcfc2796fb8
Martin Urbanec <martin@urbanec.cz>
2017-04-28 11:21:27 +0200

    Quit on Ctrl+C

--- __init__.py
+++ __init__.py
@@ -2,6 +2,12 @@
 #-*- coding: utf-8 -*-
 import crawler as c
 import checkProxy as checker
+import sys
+import signal
+
+def handleCtrlC():
+	print "Ctrl+C was pressed. Exiting. "
+	sys.exit(0)
 
 def main():
 	ip_port = c.get_ip_port()
@@ -10,4 +16,5 @@ def main():
 			print ip, port, checker.check_if_proxy_works(ip, port)
 	
 if __name__ == "__main__":
+	signal.signal(signal.SIGINT, handleCtrlC)
 	main()
